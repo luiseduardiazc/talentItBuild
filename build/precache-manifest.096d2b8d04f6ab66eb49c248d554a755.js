805e3530b96aa33cf86a7cd72dca1dfc68f28623
Luis Eduardo <luis.diaz.car@gmail.com>
2020-05-03 00:06:00 -0500

    add first commit

---
+++ build/precache-manifest.096d2b8d04f6ab66eb49c248d554a755.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32582e699df80915b65c884a4b7e19a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ff435eea4ff4e563015",
+    "url": "/static/css/2.2ee348a7.chunk.css"
+  },
+  {
+    "revision": "b91ec254832d12f0ca44",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8ff435eea4ff4e563015",
+    "url": "/static/js/2.81349936.chunk.js"
+  },
+  {
+    "revision": "d672c10fc7c11e3172e97fb010d794c6",
+    "url": "/static/js/2.81349936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b91ec254832d12f0ca44",
+    "url": "/static/js/main.be27a236.chunk.js"
+  },
+  {
+    "revision": "f065b2023d33a56b7ff7",
+    "url": "/static/js/runtime-main.7e7145f0.js"
+  }
+]);
